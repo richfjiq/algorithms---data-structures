65ab17d30fa0c3443599b6b5fcfdd33ea7a001e6
richfjiq <rfjiq1986@gmail.com>
2023-03-07 06:03:42 -0600

    multiple pointers pattern

---
+++ multiple-pointers/index.js
@@ -0,0 +1,19 @@
+const sumZero = (arr) => {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left < right) {
+    let sum = arr[left] + arr[right];
+
+    if (sum === 0) {
+      return [arr[left], arr[right]];
+    } else if (sum > 0) {
+      right--;
+    } else {
+      left++;
+    }
+  }
+};
+
+console.log(sumZero([-4, -3, -2, -1, 0, 1, 2, 3, 10])); // [-3, 3]
+console.log(sumZero([-4, -3, -2, -1, 0, 5, 6, 10])); // undefined
