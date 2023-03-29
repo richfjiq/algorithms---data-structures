84d00b3abe6f65c103e93286a107fefda1b4d067
richfjiq <rfjiq1986@gmail.com>
2023-03-29 15:27:27 -0600

    selection sort algorithm

--- searching-algorithms/bubble-sort/index.js
+++ searching-algorithms/bubble-sort/index.js
@@ -1,13 +1,17 @@
 const bubbleSort = (arr) => {
-  // for (let i = 0; arr.length < arr.length; i--) {
+  // for (let i = arr.length; i > 0; i--) {
   //   for (let j = 0; j < i - 1; j++) {
+  //     console.log(arr, arr[j], arr[j + 1]);
   //     if (arr[j] > arr[j + 1]) {
   //       let temp = arr[j];
   //       arr[j] = arr[j + 1];
   //       arr[j + 1] = temp;
   //     }
   //   }
+  //   console.log('One pass complete');
   // }
+
+  // return arr;
 
   // const swap = (arr, idx1, idx2) => {
   //   [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
@@ -15,6 +19,7 @@ const bubbleSort = (arr) => {
 
   // for (let i = arr.length; i > 0; i--) {
   //   for (let j = 0; j < i - 1; j++) {
+  //     console.log(arr, arr[j], arr[j + 1]);
   //     if (arr[j] > arr[j + 1]) {
   //       swap(arr, j, j + 1);
   //     }
@@ -29,6 +34,7 @@ const bubbleSort = (arr) => {
   for (let i = arr.length; i > 0; i--) {
     noSwaps = true;
     for (let j = 0; j < i - 1; j++) {
+      console.log(arr[j], arr[j + 1]);
       if (arr[j] > arr[j + 1]) {
         let temp = arr[j];
         arr[j] = arr[j + 1];
@@ -42,4 +48,5 @@ const bubbleSort = (arr) => {
   return arr;
 };
 
-console.log(bubbleSort([37, 45, 29, 8, 12, 88, -3])); // [8, 29, 37, 45]
+// console.log(bubbleSort([37, 45, 29, 8])); // [8, 29, 37, 45]
+console.log(bubbleSort([37, 45, 29, 8, 12, 88, -3, 11])); // [-3, 8, 12, 29, 37, 45, 88]
---
+++ searching-algorithms/selection-sort/index.js
@@ -0,0 +1,20 @@
+const selectionSort = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    let lowest = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      console.log(i, j);
+      if (arr[j] < arr[lowest]) {
+        lowest = j;
+      }
+    }
+    if (i !== lowest) {
+      let temp = arr[i];
+      arr[i] = arr[lowest];
+      arr[lowest] = temp;
+    }
+  }
+
+  return arr;
+};
+
+console.log(selectionSort([34, 22, 10, 19, 17])); // [ 10, 17, 19, 22, 34 ]
