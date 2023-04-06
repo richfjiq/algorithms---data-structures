16d8a058207d5ebd24080ae85b67398c06da5126
richfjiq <rfjiq1986@gmail.com>
2023-04-06 16:24:05 -0600

    data structures - introduction

---
+++ data-structures/introduction/index.js
@@ -0,0 +1,60 @@
+class Student {
+  constructor(firstName, lastName, year) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.grade = year;
+    this.tardies = 0;
+    this.scores = [];
+  }
+
+  fullName() {
+    return `Your full name is ${this.firstName} ${this.lastName}`;
+  }
+
+  markLate() {
+    this.tardies += 1;
+    if (this.tardies >= 3) {
+      return 'YOU ARE EXPELLED!!!';
+    }
+    return `${this.firstName} ${this.lastName} has been late ${this.tardies} times`;
+  }
+
+  addScore(score) {
+    this.scores.push(score);
+    return this.scores;
+  }
+
+  calculateAverage() {
+    let sum = this.scores.reduce((a, b) => a + b);
+    return sum / this.scores.length;
+  }
+
+  static EnrollStudents() {
+    return 'ENROLLING STUDENTS!';
+  }
+}
+
+class Point {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  static distance(a, b) {
+    const dx = a.x - b.x;
+    const dy = a.y - b.y;
+
+    return Math.hypot(dx, dy);
+  }
+}
+
+const p1 = new Point(5, 5);
+const p2 = new Point(10, 10);
+console.log('Point distance', Point.distance(p1, p2));
+
+let student = new Student('colt', 'steele', 20);
+student.addScore(98);
+student.addScore(76);
+student.addScore(100);
+console.log(student.calculateAverage());
+console.log(Student.EnrollStudents());
